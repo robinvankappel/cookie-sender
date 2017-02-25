97ad434f55c09817e11f418dc03640015e063375
j_moene <j_moene>
2017-02-25 13:28:21 +0000

    updated latest version of jelle_instance for google_instance.

---
+++ config_paths.py
@@ -0,0 +1,3 @@
+JSON_DIR = 'E:\\Users\\J. Moene\\Desktop\\CookieMonster_pythonfiles\\generated_scripts\\json_files\\temp'
+PIORESULTS_DIR = 'E:\\db-filler\\generated_scripts\\OUTPUT_results'
+WATCH_DIR = PIORESULTS_DIR
---
+++ runWatchers.bat
@@ -0,0 +1,37 @@
+start call python E:/watcher/main.py E:/db-filler/generated_scripts/OUTPUT_results/A
+
+
+
+start call python E:/watcher/main.py E:/db-filler/generated_scripts/OUTPUT_results/B
+
+
+
+start call python E:/watcher/main.py E:/db-filler/generated_scripts/OUTPUT_results/C
+
+
+
+start call python E:/watcher/main.py E:/db-filler/generated_scripts/OUTPUT_results/D
+
+
+
+start call python E:/watcher/main.py E:/db-filler/generated_scripts/OUTPUT_results/E
+
+
+
+start call python E:/watcher/main.py E:/db-filler/generated_scripts/OUTPUT_results/F
+
+
+
+start call python E:/watcher/main.py E:/db-filler/generated_scripts/OUTPUT_results/G
+
+
+
+start call python E:/watcher/main.py E:/db-filler/generated_scripts/OUTPUT_results/H
+
+
+
+start call python E:/watcher/main.py E:/db-filler/generated_scripts/OUTPUT_results/I
+
+
+
+start call python E:/watcher/main.py E:/db-filler/generated_scripts/OUTPUT_results/J
