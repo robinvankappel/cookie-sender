404cf973f2185edc8491e8b6fca502de2f5b05f0
robinvankappel@gmail.com <robinvankappel@gmail.com>
2017-10-17 20:40:08 +0100

    removed 'runWatchers'

--- runWatchers_test.bat
+++
@@ -1,5 +0,0 @@
-SET watcherdir=D:\watcher\main.py
-SET resultdir=D:\db-filler\generated_scripts\OUTPUT_results\
-start call python %watcherdir% %resultdir%A
-start call python %watcherdir% %resultdir%B
-start call python %watcherdir% %resultdir%C
